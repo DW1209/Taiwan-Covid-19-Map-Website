517b08cd27afba66e7139b67e9dd38284fc51d33
dw1209 <daniel20001209@gmail.com>
2022-11-04 23:16:06 +0800

    Move the js code in index.html to index.js

---
+++ website/static/js/index.js
@@ -0,0 +1,13 @@
+$('a').mouseenter(function() {
+    var area = (this.id).replaceAll('-', ' ')
+    var patients = records[area]
+    var info = '<h2>' + area + '</h2><h3>' + patients + '</h3>'
+    document.getElementById("info").innerHTML = info;
+
+    for (var key of Object.keys(ranges)) {
+        if (ranges[key]['from'] <= patients && patients <= ranges[key]['to']) {
+            document.getElementById(this.id).classList.add("level" + key);
+            break;
+        }
+    }
+});
